b4b2448b39ea2460bba38a0c0632ac4b80282e97
MAHRI ANTONGIOVANNI <123492202+mahriAnton@users.noreply.github.com>
2023-01-26 16:53:04 +0000

    made it work

--- BallPanel.java
+++ BallPanel.java
@@ -2,26 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 public class BallPanel extends JPanel{
-    int x = 100;
-    int y = 50;
-    int xSpeed=3;
-    int ySpeed = 1;
-    ArrayList <Ball> balls = new ArrayList<Ball> (20);
   
+   
+   ArrayList <Ball> balls = new ArrayList<Ball>(20);
+   
     
     public BallPanel(){
         setBackground(new Color(255,0,0));
+for (int i = 0; i<20;i++){
+    Ball baller = new Ball();
+    balls.add(baller);
+   }
+      
     }
     
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        for(int i = 0; i<balls.size(); i++){
+    
+  
+       
+        for(int i = 0; i<balls.size(); i++){    
           balls.get(i).drawBall(g);
           balls.get(i).moveBall(this);
+          
         }
-        repaint();
-        
-       
+      repaint();
     }
 }
